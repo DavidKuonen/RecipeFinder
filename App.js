88b0f976f5e4a24982a96b0c937e086083cee101
DavidKuonen <david.kuonen@gmail.com>
2022-09-15 15:32:35 +0300

    Exercise Complete

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import React from 'react';
+import { StyleSheet, Text, TextInput, Image, View, Button, Alert, FlatList } from 'react-native';
 
 export default function App() {
+
+const [keyword, setKeyword] = useState('');
+const [meals, setMeals] = useState([]);
+
+//Fetch API
+const fetchMeals = () =>{
+  fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i='+keyword)
+  .then(response => response.json())
+  .then(data => setMeals(data.meals))
+  .catch(err => Alert.alert('Error', err))
+};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+      data={meals}
+      renderItem={({item}) =>
+      <View style={{margin: 10}}>
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}>{item.strMeal}</Text>
+        <Image
+        style={{width: 60, height: 60, marginLeft: 5}}
+        source={{uri:item.strMealThumb}}
+        />
+      </View>
+      }
+      ItemSeparatorComponent={() => {
+        return (
+            <View style={{height: 1, backgroundColor: 'black'}}/>
+        )
+    }}
+      />
+
+      <TextInput
+      style={{fontSize: 18}}
+      placeholder='Ingredient'
+      onChangeText={text => setKeyword(text)}
+
+      />
+      <Button title='Search' onPress={fetchMeals}></Button>
+
     </View>
   );
 }
@@ -13,6 +51,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
